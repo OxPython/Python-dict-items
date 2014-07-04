d7ff4899ff78fc98349489959d3f7ef0851a5d99
Emerson <root@emerson-N53SN>
2014-07-04 05:18:46 -0500

    HowTo get all the items from a dict in python?

--- dict_items.py
+++ dict_items.py
@@ -1,37 +1,27 @@
 '''
-Created on Jul 1, 2014
+Created on Jul 2, 2014
 
 @author: viejoemer
+
+HowTo get all the items from a dict in python?
+
+¿Cómo obtener todos los elementos de un diccionario en Python?
+
 '''
-############################################################
-#HowTo get all the items from a dict in python?
-
-#Creating a empty dict
-
-d = {}
-
-d = dict()
 
 #Creating a dict with data
-
 d = {
      "red":100,
      "yellow":200,
      "blue" : 300
      }
-
 print(d)
 
 #Get the items from a dict
 items = d.items()
-
 print(type(items))
-
 print(items)
 
+#Loop the data
 for key, value in items:
     print(key , value)
-
-print(d)
-
-print(d)
